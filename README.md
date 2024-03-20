294d5b12106bb9fb7204af0d037375bc00dfa9da
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-03-20 21:06:46 +0300

    dart-functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dart
+# dart code samples on functions
+1. The first block of code has a function that invokes each fruit without having a function name.
---
+++ samples.dart
@@ -0,0 +1,8 @@
+// This function invokes each fruit without having a function name.
+void main() {
+  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
