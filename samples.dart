f7d96ee955aa977ff3a8a91cc316c00d7faec876
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-03-23 20:33:01 +0300

    work with classes

--- samples.dart
+++ samples.dart
@@ -35,14 +35,49 @@
 // }
 
 // arrow function that calculate interest
-double calculateInterest(double principal, double rate, double time) =>
-    principal * rate * time / 100;
+// double calculateInterest(double principal, double rate, double time) =>
+//     principal * rate * time / 100;
 
-void main() {
-  double principal = 100000;
-  double time = 9.5;
-  double rate = 3;
+// void main() {
+//   double principal = 100000;
+//   double time = 9.5;
+//   double rate = 3;
 
-  double result = calculateInterest(principal, rate, time);
-  print("The simple interest is $result.");
-}
+//   double result = calculateInterest(principal, rate, time);
+//   print("The simple interest is $result.");
+// }
+
+// Object Oriented Programming in Dart
+// class Dog {
+//   // Properties/attributes
+//   String breed;
+//   String color;
+//   String origin;
+
+//   // Constructor
+//   Dog(this.breed, this.color,this.origin);
+
+//   // Methods/behaviours
+//   void bark() {
+//     print('Woof!');
+//   }
+
+//   void run() {
+//     print('The dog is running.');
+//   }
+// }
+
+// void main() {
+//   // Creating an instance of the Dog class/object
+//   var myDog = Dog('Labrador', 'Golden','Siberian');
+
+//   // Accessing properties/attributes/characteristics
+//   print('Breed: ${myDog.breed}');
+//   print('Color: ${myDog.color}');
+//   print('Origin:${myDog.origin}');
+
+//   // Calling methods
+//   myDog.bark();
+//   myDog.run();
+// }
+
